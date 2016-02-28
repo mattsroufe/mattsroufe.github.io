5f2e404dd796216b49c5f085e0ae3a69859360ec
Matt Sroufe <mattsroufe@gmail.com>
2016-02-28 20:48:06 +1300

    uses es6 class for prototypes

--- chess/js/square.js
+++ chess/js/square.js
@@ -1,35 +1,48 @@
-Square = (function () {
-  return {
-    new: function (el) {
-      el.addEventListener('dragover', dragover);
-      el.addEventListener('drop', drop);
-      el.contains = function (color) {
-        return this.isEmpty ? false : this.piece.color() === color;
-      };
+document.registerElement('chess-square', {
+  extends: 'td',
+  prototype: Object.create(HTMLElement.prototype, {
+    createdCallback: {
+      value: function() {
+        function dragover(e) {
+          e.preventDefault();
+          e.dataTransfer.dropEffect = 'move';
+        };
 
-      Object.defineProperties(el, {
-        "rank": {
-          get: function () {
-            return parseInt(this.id.charAt(1));
-          }
-        },
-        "file": {
-          get: function () {
-            return this.id.charAt(0);
-          }
-        },
-        "piece": {
-          get: function () {
-            return this.firstChild;
-          }
-        },
-        "isEmpty": {
-          get: function () {
-            return !this.hasChildNodes();
-          }
-        }
-      });
-      return el;
+        function drop(e) {
+          e.preventDefault();
+          var last_position = e.dataTransfer.getData("text");
+          var new_position  = (e.target.id.length > 0) ? e.target.id : e.target.parentNode.id;
+          console.log(last_position, new_position);
+        };
+
+        this.addEventListener('dragover', dragover);
+        this.addEventListener('drop', drop);
+      }
+    },
+    contains: {
+      value: function (color) {
+        return this.isEmpty ? false : this.piece.color() === color;
+      }
+    },
+    rank: {
+      get: function () {
+        return parseInt(this.id.charAt(1));
+      }
+    },
+    file: {
+      get: function () {
+        return this.id.charAt(0);
+      }
+    },
+    piece: {
+      get: function () {
+        return this.firstChild;
+      }
+    },
+    isEmpty: {
+      get: function () {
+        return !this.hasChildNodes();
+      }
     }
-  }
-})();
+  })
+});
